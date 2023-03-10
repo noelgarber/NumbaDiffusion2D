9324a48961515cc8774cccef5d3af789a632c008
noelgarber <104410131+noelgarber@users.noreply.github.com>
2023-03-09 20:22:40 -0500

    Replaced kernel logic
    Works now, but very slow!

--- diff2d_gpu.py
+++ diff2d_gpu.py
@@ -14,18 +14,28 @@ import time
 from numba import cuda
 from diff2dplot import plotdens
 
-# cuda kernel
-@cuda.jit('void(int32, int32, float32, float32, float32, float32[:,:], float32[:,:], float32[:,:])')
-def update_density(gpu_nrows, gpu_ncols, gpu_D, gpu_dx, gpu_dt, gpu_laplacian, gpu_dens, gpu_densnext):
-    i, j = cuda.grid(2)
-    if i >= 1 and i <= gpu_nrows and j >= 1 and j <= gpu_ncols:
-        gpu_laplacian_ij = gpu_dens[i + 1, j] + gpu_dens[i - 1, j] + gpu_dens[i, j + 1] + gpu_dens[i, j - 1] - 4 * gpu_dens[i, j]
-        gpu_laplacian[i,j] = gpu_laplacian_ij
-        gpu_densnext[i,j] = gpu_dens[i,j] + (gpu_D / gpu_dx ** 2) * gpu_dt * gpu_laplacian_ij
+'''
+Define a CUDA kernel that evolves the density array. As input, it takes the following variables, stored in device memory: 
+    input_dens_dev:         the 2D density array for the previous time step
+    output_laplacian_dev:   a blank 2D array of equal shape to the density array; 
+                            note that the laplacian from the previous time step is not taken as an argument and thus not required
+    output_densnext_dev:    a blank 2D array of equal shape to the density array; used to output the newly evolved density
+    nrows_dev:              the number of rows to iterate over
+    ncols_dev:              the number of cols to iterate over
+    D_dev, dx_dev, dt_dev:  input parameters
+'''
+@cuda.jit('void(float64[:,:], float64[:,:], float64[:,:], int32, int32, float64, float64, float64)')
+def evolve_density_kernel(input_dens_dev, output_laplacian_dev, output_densnext_dev, nrows_dev, ncols_dev, D_dev, dx_dev, dt_dev):
+    for i in range(1, nrows_dev + 1):
+        for j in range(1, ncols_dev + 1):
+            output_laplacian_dev[i][j] = input_dens_dev[i+1][j] + input_dens_dev[i-1][j] + input_dens_dev[i][j+1] + input_dens_dev[i][j-1] - 4*input_dens_dev[i][j]
+    for i in range(1, nrows_dev + 1):
+        for j in range(1, ncols_dev + 1):
+            output_densnext_dev[i][j] = input_dens_dev[i][j] + (D_dev/dx_dev**2)*dt_dev*output_laplacian_dev[i][j]
 
 # driver routine
 def main():
-    t1 = time.time()
+    print("Beginning simulation...")
 
     # Script sets the parameters D, x1, x2, runtime, dx, outtime, and graphics:
     from diff2dparams import D, x1, x2, runtime, dx, outtime, graphics
@@ -34,98 +44,65 @@ def main():
     nrows     = int((x2-x1)/dx) # number of x points
     ncols     = nrows           # number of y points, same as x in this case.
     npnts     = nrows + 2       # number of x points including boundary points
-    dx_recalc = (x2-x1)/nrows   # recompute (previous dx may not fit in [x1,x2])
-    dt        = 0.25*dx_recalc**2/D    # time step size (edge of stability)
+    dx        = (x2-x1)/nrows   # recompute (previous dx may not fit in [x1,x2])
+    dt        = 0.25*dx**2/D    # time step size (edge of stability)
     nsteps    = int(runtime/dt) # number of steps of that size to reach runtime
-    nper      = int(outtime/dt) # how many step s between snapshots
+    nper      = int(outtime/dt) # how many steps between snapshots
     if nper==0: nper = 1
 
-    t2 = time.time()
-    print("Time taken computing derived parameters:", t2 - t1, "s")
-
-    # Initialize general device variables
-    nrows_dev = cp.int32(nrows)
-    ncols_dev = cp.int32(ncols)
-    D_dev = cp.float32(D)
-    dx_dev = cp.float32(dx_recalc)
-    dt_dev = cp.float32(dt)
-
-    # Declare device parameters
-    max_threads_per_block = cuda.get_current_device().MAX_THREADS_PER_BLOCK
-    threadsperblock = max_threads_per_block
-    blockspergrid = (nrows_dev + threadsperblock - 1) // threadsperblock
-
-    t3 = time.time()
-    print("Time taken computing general GPU-related variables:", t3 - t2, "s")
+    print("\tNumber of steps:", nsteps)
+    print("\tSteps per snapshot:", nper)
 
     # Allocate arrays.
-    x = np.array([x1+((i-1)*(x2-x1))/nrows for i in range(npnts)]) # x values (also y values)
-    dens = np.zeros((npnts, npnts)) # time step t
-    densnext = np.zeros((npnts, npnts)) # time step t+1
+    x         = [x1+((i-1)*(x2-x1))/nrows for i in range(npnts)] # x values (also y values)
+    dens      = [[0.0]*npnts for i in range(npnts)] # time step t
+    densnext  = [[0.0]*npnts for i in range(npnts)] # time step t+1
 
     # Initialize.
     simtime=0*dt
-    for i in np.arange(1,npnts-1):
+    for i in range(1,npnts-1):
         a = 1 - abs(1 - 4*abs((x[i]-(x1+x2)/2)/(x2-x1)))
-        for j in np.arange(1,npnts-1):
+        for j in range(1,npnts-1):
             b = 1 - abs(1 - 4*abs((x[j]-(x1+x2)/2)/(x2-x1)))
             dens[i][j] = a*b
-
-    t4 = time.time()
-    print("Time spent during initialization:", t4 - t3, "s")
 
     # Output initial signal.
     print(simtime)
     if graphics:
         plotdens(dens, x[0], x[-1], first=True)
 
-    t5 = time.time()
-    print("Time taken outputting initial signal:", t5 - t4, "s")
+    # copy the initialized density array; becomes 64-bit float array in device memory
+    dens_dev = cp.array(dens)
 
-    # Initialize step-specific device variables to input into the kernel.
-    laplacian_dev = cp.zeros((npnts, npnts), dtype=np.float32)  # temporary device array to hold the laplacian
-    dens_dev = cp.asarray(dens, dtype=cp.float32)
-    densnext_dev = cp.asarray(densnext, dtype=cp.float32)
+    # perform the computationally heavy component on the GPU as follows
+    for s in range(nsteps):
+        print("Step", s, "of", nsteps)
 
-    t6 = time.time()
-    print("Time taken transferring arrays to device memory:", t6 - t5, "s")
+        # redefine output arrays as blanks
+        output_laplacian_dev = cp.zeros((npnts, npnts)) # temporary array to hold the laplacian; default 64-bit floats
+        output_densnext_dev = cp.zeros((npnts, npnts)) # temporary array to hold the newly evolved density; default 64-bit floats
 
-    #TODO: The erasure of dens/dens_dev occurs at some point after this
+        # define block and grid dimensions to fit the data
+        blockDim = (32, 32)
+        gridDim = ((nrows + blockDim[0] - 1) // blockDim[0], (ncols + blockDim[1] - 1) // blockDim[1])
 
-    for s in np.arange(nsteps):
-        print("Current Step:", s)
-        t01 = time.time()
+        # execute the kernel
+        evolve_density_kernel[gridDim, blockDim](dens_dev, output_laplacian_dev, output_densnext_dev, nrows, ncols, D, dx, dt)
 
-        # Take one step to produce new density.
-        update_density[blockspergrid, threadsperblock](nrows_dev, ncols_dev, D_dev, dx_dev, dt_dev,
-                                                       laplacian_dev, dens_dev, densnext_dev)
+        # copy the results to become the new density array on device memory
+        dens_dev = output_densnext_dev.copy()
+        simtime += dt # update the simulation time
 
-        t02 = time.time()
-        #print("\tTime taken executing the update_density GPU kernel:", t02 - t01, "s")
+        # free up device memory before the next round
+        del output_densnext_dev, output_laplacian_dev
 
-        # Retrieve the new density at t+1 and assign so it becomes the new t, and update simulation time.
-        dens_dev = densnext_dev
-        dens = cp.asnumpy(dens_dev)
-        simtime += dt
         # Plot and report time.
         if (s+1)%nper == 0:
             print(simtime)
             if graphics:
-                plotdens(dens, x[0], x[-1])
-
-        t03 = time.time()
-        #print("\tTime taken preparing for another step:", t03 - t02, "s")
+                print("Plotting graphics...")
+                dens_host = cp.asnumpy(dens_dev)
+                plotdens(dens_host, x[0], x[-1])
 
 if __name__ == '__main__':
     main()
-
-
-
-'''
-for i in range(1,nrows+1):
-    for j in range(1,ncols+1):
-        laplacian[i][j] = dens[i+1][j]+dens[i-1][j]+dens[i][j+1]+dens[i][j-1]-4*dens[i][j]
-for i in range(1,nrows+1):
-    for j in range(1,ncols+1):
-        densnext[i][j] = dens[i][j] + (D/dx**2)*dt*laplacian[i][j]
-'''
